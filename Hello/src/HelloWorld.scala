eb872275082abdfb8e5a23bdfa96455d9ec69717
Sania Arif <sania.arif@columbia.edu>
2016-06-08 23:38:52 -0400

    hello world

--- Hello/src/HelloWorld.scala
+++ Hello/src/HelloWorld.scala
@@ -3,19 +3,5 @@
   */
 object HelloWorld {
   def main (args:Array[String]) {
-    var x = 10
-    var y = 150
-    var z = add(x,y)
-    printme("Result is " + z)
-  }
-
-  def add (a: Int, b: Int) : Int = {
-
-    return a + b
-  }
-
-  def printme (s: String) : Unit = {
-
-    println(s)
-  }
+    println("Hello World")
 }
