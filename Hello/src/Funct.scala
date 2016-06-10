0ed13b3a33e7f7003f76baa565833a2d3efbd8c1
Sania Arif <sania.arif@columbia.edu>
2016-06-09 22:03:14 -0400

    Demonstrating call by name

---
+++ Hello/src/Funct.scala
@@ -0,0 +1,26 @@
+/**
+  * Created by sania.
+  */
+import java.util.Date;
+
+object Funct {
+  def main(args: Array[String]) {
+    println("Call by Name:")
+    println("Returned value is: " + delayed(time()));
+    println()
+  }
+
+  /*
+  Demonstrating call-by-name
+  -- passes a code block to the callee and each time the callee accesses the parameter,
+     the code block is executed and the value is calculated.
+  */
+  def delayed(t: => Long) = {
+    //takes a call-by-name param represented by =>
+    println("In delayed method")
+    println("Param: " + t)
+    t //Return value
+  }
+
+
+}
